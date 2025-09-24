f44aa82fa8d323459abf50c72b3592064a17db15
erik <erlo22@student.bth.se>
2025-09-24 10:34:35 +0200

    text changes to front-page

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
   const MetaData = {
     title: "Nordiskdev",
     description: "Vi är Nordiskdev, din techpartner i södra Sverige. Vi erbjuder skräddarsydda lösningar inom apputveckling, webbutveckling och konsulttjänster.",
-    Apputveckling: "Mer information om apputveckling kommer snart.",
-    Webbutveckling: "Mer information om webbutveckling kommer snart.",
-    konsultering: "Mer information om konsulttjänster kommer snart.",
+    Apputveckling: "Om du någonsin tänkt att detta skulle man haft en app till så kan göra tanken till verklighet. Vi erbjuder apputveckling för både Android och Iphone.",
+    Webbutveckling: "Är ni ett nystartat bolag som behöver ny hemsida? Kanske är er gamla sida grå och trist? Vi hjälper er skapa en ny, fräsch sida!",
+    konsultering: "Vilken programvara är bäst för att lösa just era problem? Genom vår konsultering hittar vi en lösning i er prisklass",
     backgroundImage: "/björkis.jpg",
   };
 
@@ -24,7 +24,7 @@ export default function Home() {
 
   return (
     <div className="" >
-      <Hero title='ER VISION' span='VÅR UTMANING' subheading='Din Techpartner I Södra Sverige'/>
+      <Hero title='ER VISION' span='VÅR UTMANING' subheading='Vi finns med dig, från problem till lösning'/>
       <Sektion data={MetaData} />
       <Partner />
       {/* <ResourceSektion /> */}
