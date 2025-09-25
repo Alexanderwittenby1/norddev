356e5910c9e0f1a51f438af8440d22602e6d796c
erik <erlo22@student.bth.se>
2025-09-25 11:05:17 +0200

    Adding kontakt and sociala medier to footer

--- components/custom/footer.tsx
+++ components/custom/footer.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
-
+import Link from "next/link";
+import Image from "next/image";
 export const Footer = () => {
   return (
     <div >
       <footer className="bg-[url(/grain.jpg)] bg-cover text-white py-4 flex flex-col justify-end items-start min-h-[160px]">
-        <div className="container mx-auto flex flex-col items-start space-y-2">
-          <p className="text-sm">
-            &copy; {new Date().getFullYear()} NordDev. All rights reserved.
-          </p>
-          <a href="/privacy-policy" className="text-sm hover:underline">
-            Integritetspolicy
-          </a>
+        <div className='w-full max-w-7xl mx-auto px-4 2xl:px-0'>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+            <div className="container mx-auto flex flex-col items-start space-y-2">
+              <p className="text-sm">
+                &copy; {new Date().getFullYear()} NordDev. All rights reserved.
+              </p>
+              <a href="/privacy-policy" className="text-sm hover:underline">
+                Integritetspolicy
+              </a>
+            </div>
+            <div className="container mx-auto flex flex-col items-start space-y-2">
+            <h1><u>Kontakt</u></h1>
+            <a href="mailto:example@example.com">info@nordiskdev.se</a>
+              <a href="tel:0457-xxxxxx"> 0457-xxxxxx </a>
+            </div>
+            <div className="container mx-auto flex flex-col items-start space-y-2">
+              <h1><u>Social medier</u></h1>
+              <a href="https://www.facebook.com"> Facebook </a>
+              <a href="https://www.linkedin.com"> Linkedin </a>
+            </div>
+          </div>
         </div>
       </footer>
     </div>
