eabf71c9b1a49b58c22522010128abc6a0ea37cd
Alexanderwittenby1 <allewi@live.se>
2025-10-01 11:57:17 +0200

    fix: det var fel konto/lösen

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,7 +6,6 @@ export async function POST(request: Request) {
 
   const { firstname,lastname, email, message } = await request.json();
   
-  
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: 465,
@@ -18,8 +17,6 @@ export async function POST(request: Request) {
   });
 
   try {
-    const html = emailToTeam({ firstname, lastname, email, message });
-
     await transporter.sendMail({
       from: `Nytt kundmejl <kontakt@nordiskdev.se>` ,
       to: 'kontakt@nordiskdev.se',
